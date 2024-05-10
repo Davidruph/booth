bd2de588628e473e4d82126c3982cecbed70374a
Agbugba David C. <dagbugba@yahoo.com>
2024-05-10 11:16:40 +0100

    server modification

--- server/index.js
+++ server/index.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const cors = require("cors"); // Import cors package
+const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
-const port = 3001;
-const bodyParser = require("body-parser");
+const port = process.env.PORT || 3001; // Use PORT environment variable for Vercel
 
-// Parse application/json
-app.use(bodyParser.json());
-
-// Middleware to enable CORS
+// Middleware
 app.use(cors());
+app.use(express.json());
 
 // Multer storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "public/uploads/"); // Adjust destination path
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
@@ -36,13 +33,13 @@ app.post("/upload", upload.array("files"), (req, res) => {
   res.send("Files uploaded successfully");
 });
 
-// Serve static files from the 'upload' folder
-app.use("/uploads", express.static(path.join(__dirname, "upload")));
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, "public")));
 
-// Endpoint to list image files in the 'upload' folder
+// Endpoint to list image files in the 'uploads' folder
 app.get("/images", async (req, res) => {
   try {
-    const files = await fs.readdir(path.join(__dirname, "upload"));
+    const files = await fs.readdir(path.join(__dirname, "public/uploads"));
     const imageUrls = files
       .filter((file) => file.match(/\.(jpg|jpeg|png|gif)$/i))
       .map((file) => `/uploads/${file}`);
